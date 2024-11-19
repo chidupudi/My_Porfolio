061dcc2f50c1d8d2cd601678c45a6608b6a47876
Rupesh Chidupudi <chrupesh2425@gmail.com>
2024-11-19 21:36:39 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@
     let menu = document.querySelector('#menu-icon');
     let navbar = document.querySelector('.navbar');
      
-     
     window.addEventListener('scroll', () => {
         header.classList.toggle('shadow', window.scrollY > 0);
     });
